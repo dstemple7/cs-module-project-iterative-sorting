6c135c36cd360734480644448cb763986e4a35cc
David Temple <dstemple7@gmail.com>
2020-09-15 11:55:54 -0500

    added sean's solution code & stretch solve

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -19,6 +19,27 @@ def selection_sort(arr):
 
     return arr
 
+    # # Sean's solution code
+    # def selection_sort(arr):
+    #     # loop through n-1 elements
+    #     for i in range(0, len(arr) - 1):
+    #         smallest_index = i
+    #         # TO-DO: find the smallest element
+    #         # (hint, can do in 3 loc)
+    #         # loop through every elem to the right of the boundary and keep track of the smallest elem
+    #         # we've seen so far until we get to the end
+    #         for j in range(i+1, len(arr)):
+    #             if arr[j] < arr[smallest_index]:
+    #                 smallest_index = j
+
+    #         # TO-DO: swap
+    #         arr[smallest_index], arr[i] = arr[i], arr[smallest_index]
+                # # under the hood, python is doing this
+                # temp = arr[smallest_index]
+                # arr[smallest_index] = arr[i]
+                # arr[i] = temp
+
+    #     return arr
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
@@ -33,6 +54,18 @@ def bubble_sort(arr):
                 arr[h] = swap
 
     return arr
+
+# # sean's solution code
+# def bubble_sort(arr):
+#     # keep a flag that tracks whether any swaps occurred
+#     swaps_occurred = True
+#     while swaps_occurred:
+#         swaps_occurred = False
+#         for i in range(len(arr)-1):
+#             if arr[i] > arr[i+1]:
+#                 arr[i], arr[i+1] = arr[i+1], arr[i]
+#                 swaps_occurred = True
+#     return arr
 
 '''
 STRETCH: implement the Counting Sort function below
@@ -53,6 +86,30 @@ What is the time and space complexity of the counting sort algorithm?
 '''
 def counting_sort(arr, maximum=None):
     # Your code here
+    if len(arr) == 0:
+        return arr
+    
+    if maximum is None:
+        maximum = max(arr)
 
+    buckets = [0 for i in range(maximum+1)]
 
-    return arr
+    # loop through our arr
+    for value in arr:
+        if value < 0:
+            return "Error, negative numbers not allowed in Count Sort"
+        # for each distinct arr value, increment arr[value] by 1
+        buckets[value] += 1
+
+    # at this point, our buckets array has all of the counts of every distinct value in our input array
+
+    output = []
+    # loop through our buckets arry
+    # enumerate gives index as well as the bucket value
+    for index, count in enumerate(buckets):
+        # for the current count
+        output.extend([index for i in range(count)])
+        # add that many values to an output array
+
+    return output
+
